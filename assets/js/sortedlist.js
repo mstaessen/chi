7a8a036707181c8260b5e9d73fc0ea051a7a8bf5
Bram Gotink <bram.gotink@vtk.be>
2012-05-09 14:49:47 +0200

    added some extra functions

--- assets/js/sortedlist.js
+++ assets/js/sortedlist.js
@@ -2,15 +2,14 @@
  * sort_function:
  * sort_function(a,b) > 0 if a comes before b
  */
-function SortedList(sort_function) {
+function SortedList(sort_function, eq_function) {
     this.root = 0;
-    this.end = 0;
     this.sf = sort_function;
+    this.ef = eq_function || function(a,b) { return a == b; };
 }
 
 SortedList.Element = function(elem) {
     this.e = elem;
-    this.p = 0;
     this.n = 0;
 }
 
@@ -18,12 +17,10 @@ SortedList.prototype.add = function (elem) {
     var newElem = new SortedList.Element(elem);
     if (!this.root) {
         this.root = newElem;
-        this.end = this.root;
         return;
     }
     
     if (this.sf(this.root.e, elem) <= 0) {
-        this.root.p = newElem;
         newElem.n = this.root;
         
         this.root = newElem;
@@ -38,21 +35,52 @@ SortedList.prototype.add = function (elem) {
     }
     
     if (!cur) {
-        if (prev != this.end) console.log('something went wrong, SortedList is corrupted');
-    
         prev.n = newElem;
-        newElem.p = prev;
-        
-        this.end = newElem;
         return;
     }
-    var tmp = cur ? cur.n : 0;
+    
+    var tmp = cur.n;
     
     if (cur) cur.n = newElem;
-    newElem.p = cur;
-    
-    if (tmp) tmp.p = newElem;
     newElem.n = tmp;
+}
+
+SortedList.prototype.remove = function(elem) {
+    var cur = this.root;
+    var prev = false;
+    
+    while (cur && !this.ef(cur.e, elem)) {
+        prev = cur;
+        cur = cur.n;
+    }
+    
+    if (!cur) return false;
+    if (!this.ef(cur.e, elem)) return false;
+    
+    if (prev)
+        prev.n = cur.n;
+    else
+        this.root = cur.n;
+    
+    return cur.e;
+}
+
+// fn_match(e) returns true if e should be removed
+SortedList.prototype.removeAll = function(fn_match) {
+    var cur = this.root;
+    var prev = false;
+    while (cur) {
+        if (fn_match(cur.e)) {
+            if(prev) {
+                prev.n = cur.n;
+            } else {
+                this.root = cur.n;
+            }
+        }
+        
+        prev = cur;
+        cur = cur.n;
+    }
 }
 
 SortedList.prototype.foreach = function(fun) {
@@ -70,6 +98,11 @@ SortedList.prototype.map = function(fun) {
         result.push(fun(cur.e));
         cur = cur.n;
     }
+    return result;
+}
+
+SortedList.prototype.clear = function() {
+    this.root = 0;
 }
 
 var SortFunctions = {};
@@ -80,4 +113,10 @@ SortFunctions.date = function(a,b) {
 
 SortFunctions.readLater = function(a,b) {
     return Date.parse(b.read_later) - Date.parse(a.read_later);
+}
+
+var EqualFunctions = {};
+
+EqualFunctions.msg = function(a,b) {
+    return a.id == b.id;
 }
