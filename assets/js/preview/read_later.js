f68b8ee0edeb9c36ff43ffb1f4877427cf34af21
Tom De Bie <debietom@gmail.com>
2012-05-13 17:07:02 +0200

    labels aanpassen wanneer readlater+inbox drop

--- assets/js/preview/read_later.js
+++ assets/js/preview/read_later.js
@@ -170,13 +170,13 @@ function setReadLater(undo) {
         storage.saveMessage(msg);
         if (msg.location != 'readlater') {
             setLocation(_active_item, 'readlater');
-	    preview_labels(msg)
+	    preview_labels( getMessage(_active_item));
         }
     } else {
         msg.read_later = -1;
         storage.saveMessage(msg);
         setLocation(_active_item, 'inbox'); // check dit
-	preview_labels(msg)
+	preview_labels( getMessage(_active_item));
     }
 }
 
